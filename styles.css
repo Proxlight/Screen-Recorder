3154f46b26c1792f5d951f9180d645b832aac56a
Proxlight <69844328+Proxlight@users.noreply.github.com>
2021-05-09 14:38:23 +0530

    Add files via upload

---
+++ styles.css
@@ -0,0 +1,16 @@
+body {
+  text-align: center;
+}
+
+button {
+  display: inline-block;
+  margin: 1em 1em;
+  font-size: 2em;
+  cursor: pointer;
+}
+
+video {
+  border: 1px solid lightgray;
+  width: 100%;
+  background-color: #eee;
+}
